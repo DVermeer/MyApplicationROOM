bf4caf5256a29a832f14a2b70b24913ecc1add2d
DVermeer <44090457+DVermeer@users.noreply.github.com>
2019-09-13 13:17:55 +0200

    Start- up with Room Database

--- app/src/main/java/com/example/myapplicationroom/MainActivity.java
+++ app/src/main/java/com/example/myapplicationroom/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplicationroom;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,12 +8,24 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    RecyclerView recylcerview;
+    RecyclerView.Adapter adapter;
+    FloatingActionButton fab;
+    ArrayList<String> beers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,35 +34,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        recylcerview = findViewById(R.id.recycler_view);
+
+        beers = new ArrayList<>();
+        for (int i = 0; i < 100 ; i++) {
+            beers.add("Grimbergen # " + i);
+                    }
+
+        recylcerview.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter(beers);
+        recylcerview.setAdapter(adapter);
+
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, CreateUser.class));
+
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
+
